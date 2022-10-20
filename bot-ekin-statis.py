693b21340be89fade88ebb9e8fc827cb3646de82
bg-dw <bg.dwik@gmail.com>
2022-10-20 16:00:11 +0700

    Penyempurnaan pembacaan koding

--- bot-ekin-statis.py
+++ bot-ekin-statis.py
@@ -1,5 +1,9 @@
 # from logging import exception
+<<<<<<< HEAD
 
+=======
+# library selenium untuk komunikasi dengan browser
+>>>>>>> cc7fc43 (Penyempurnaan pembacaan koding)
 from selenium import webdriver
 
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,6 +12,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 
+<<<<<<< HEAD
 from openpyxl import load_workbook
 import time
 
@@ -37,18 +42,66 @@ print(row_count)
 i = 2
 
 while i <= row_count:
+=======
+# library openpyxl untuk mengelola file excel
+from openpyxl import load_workbook
+import time
+
+# file yang akan dibaca oleh python
+wb = load_workbook(filename="C:\laragon\www\python\ekin.xlsx")
+
+sheet = wb['Sheet1']  # sheet yang dibaca
+row_count = len(sheet['A'])  # jumlah baris pada sheet excel
+
+# put your web driver into directory "C:\Windows\webdriver.exe"
+driver = webdriver.Chrome()
+
+# url tujuan untuk input data
+driver.get("https://ekinerja.situbondokab.go.id/")
+driver.maximize_window()
+driver.implicitly_wait(10)
+
+time.sleep(1)  # delay sebelum eksekusi perintah selanjutnya
+driver.find_element(
+    'id', 'login-form__username').send_keys("197102151990071001")  # silahkan ganti xxxx dengan username ekin anda
+time.sleep(1)
+driver.find_element(
+    'id', 'login-form__password').send_keys("12344321")  # silahkan ganti yyyy dengan password ekin anda
+time.sleep(1)
+driver.find_element(
+    By.XPATH, '//*[@id="login-form"]/button').click()
+
+# alamat tujuan setelah login
+driver.get("https://ekinerja.situbondokab.go.id/page/aktivitas-harian.html")
+
+# perulangan input
+i = 2
+
+while i <= row_count:  # mengulang sebanyak jumlah baris pada excel worksheet
+>>>>>>> cc7fc43 (Penyempurnaan pembacaan koding)
     tgl = sheet['A'+str(i)].value
     akt = sheet['B'+str(i)].value
     keg = sheet['C'+str(i)].value
     ctt = sheet['D'+str(i)].value
 
     try:
+<<<<<<< HEAD
         if ((i-1) != row_count):
             driver.find_element(
                 By.XPATH, '//*[@id="main-content"]/div[1]/div[2]/article/div/div/div[2]/button').click()
             WebDriverWait(driver, 15).until(EC.visibility_of_element_located(
                 (By.XPATH, '//*[@id="aktivitas-harian-modal"]/div/div')))
 
+=======
+        if ((i-1) != row_count):  # jika belum mencapat baris terakhir
+            driver.find_element(
+                By.XPATH, '//*[@id="main-content"]/div[1]/div[2]/article/div/div/div[2]/button').click()  # klik button untuk menampilkan modal tambah data
+
+            WebDriverWait(driver, 15).until(EC.visibility_of_element_located(
+                (By.XPATH, '//*[@id="aktivitas-harian-modal"]/div/div')))  # beralih pada modal
+
+            # mengirim data excel ke form input
+>>>>>>> cc7fc43 (Penyempurnaan pembacaan koding)
             driver.find_element('id', 'tgl_aktivitas').send_keys(tgl)
             ak = Select(driver.find_element(By.ID, 'aktivitasid'))
             ak.select_by_value(str(akt))
@@ -59,6 +112,7 @@ while i <= row_count:
             driver.find_element('id', 'save-aktifitas').click()
 
             time.sleep(1)
+<<<<<<< HEAD
             driver.refresh()
         else:
             break
@@ -69,6 +123,18 @@ while i <= row_count:
 
     x = i-2
     time.sleep(2)
+=======
+            driver.refresh()  # refresh halaman setelah input data
+        else:
+            break  # menghentikan program
+    except TimeoutException:  # jika terjadi kegagalan
+        print("Gagal")
+
+    i += 1
+    x = i-2
+    time.sleep(2)
+    # mengetahui banyak data yang telah diinputkan
+>>>>>>> cc7fc43 (Penyempurnaan pembacaan koding)
     print("Input data ke-"+(str(x))+" selesai!")
     if (x+1 == row_count):
         print("Semua data berhasil disimpan!")
